53ae3290461681b5bb6199c797e02a4253b160d5
Khvorost Danilo <danilo.khvorost@gmail.com>
2021-11-15 18:58:14 +0200

    Добалена структура AnimHorizDecal - анимирует  картинку с помощью смены кадров.

--- QuantumRunner/QuantumRunner.cpp
+++ QuantumRunner/QuantumRunner.cpp
@@ -1,11 +1,35 @@
-﻿// Запуск программы : CTRL + F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-#define OLC_PGE_APPLICATION
+﻿#define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 #include<iostream>
 
 using namespace std;
+
+struct AnimHorizDecal {
+    olc::Decal* img;    //  Переменная типа Декаль 
+    olc::vf2d pos;      //  Вектор с коорнидатами
+    olc::vf2d size;     //  Вектор с  размером
+    int max_frame;      //  Макс. кол-во кадров
+    float fps;          //  Количество кадров в секунду
+
+    int frame = 0;                      //  Счетчик кадров
+    olc::vf2d scale = { 2.0f, 2.0f };   //  Масштаб вектора
+
+    float _currFps = 0;                 //  Текущий фпс
+    olc::vf2d _currFrame = { 0, 0 };    //  Текущий кадр 
+
+    olc::vf2d _shift = { 1, 0 };        //  Смещение 
+
+    void update(float deltaT) {                 //  Фунуция обновления кадров
+        _currFrame = size * _shift * frame; //  Расчёт текущего кадра 
+        if (frame >= max_frame)
+            frame = 0;
+        if (_currFps > fps) {
+            _currFps = 0;
+            frame++;
+        }
+        _currFps += deltaT;
+    }
+};
 
 class QuantumRunner : public olc::PixelGameEngine
 {
@@ -13,7 +37,7 @@ public:
 	QuantumRunner()
 	{
 		
-		sAppName = "QuantumRunner";
+		sAppName = "QuantumRunner";	//	Название проэкта.
 	}
 
 public:
